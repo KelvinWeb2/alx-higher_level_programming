083eee330401ac7db9c1c220753de7b31d6aaf8c
KelvinWeb2git config --global user.name KelvinWeb2 <kelvinimere@gmail.comkelvinimere@gmail.comgit config --global user.email kelvinimere@gmail.com>
2024-03-29 01:44:06 +0000

    0x01-python-if_else_loops_functions

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
-for j in range(ord('a'), ord('z')+1):
-    if j != ord('e') and j != ord('q'):
-        print("{}".format(chr(j)), end='')
+for i in range(ord('a'), ord('z')+1):
+    if i != ord('e') and i != ord('q'):
+        print("{}".format(chr(i)), end='')
